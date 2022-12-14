4719a58698d2fe470569f0f28cb6c56117012d00
EunSuJeong99 <90062342+EunSuJeong99@users.noreply.github.com>
2022-12-14 19:56:00 +0900

    은수 index.html
    DB 연결(migrate)
    index.html, style.css 생성

--- subhap/views.py
+++ subhap/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Employees
 
 # Create your views here.
+
+def home(request):
+
+    employees = Employees.objects.all()
+
+    context = {
+        'employees' : employees,
+    }
+
+    return render(request, 'index.html', employees)
